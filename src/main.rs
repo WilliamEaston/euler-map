a3907df993c96bbbdfa5258d3247a39bf7dd26dd
William Easton <wlmestn@gmail.com>
2023-11-14 12:59:51 -0800

    Integral Cache
    Dramatically improved performance and thus resolution

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use tiny_skia::*;
 use gkquad::single::{Integrator, Points};
 
 fn main() {
-    let size = 30000.0;
-    let focused_area_size = 1000.0;
-    let g = 0.05;
+    let size = 1000000.0;
+    let focused_area_size = 3000.0;
+    let g = 0.001;
     let max_lat = 85.0;
-    let dots_per_degree = 3.0;
+    let dots_per_degree = 15.0;
 
     let t_upto = 360.0 * max_lat / g;
     let num_dots = (t_upto * dots_per_degree) as i32;
@@ -23,6 +23,7 @@ fn main() {
 
             let mercator =  Pixmap::load_png(".\\wrld-21.png").unwrap();
             let mut points: Vec<(f32, f32, u8, u8, u8)> = vec![];
+            let mut integral_cache = IntegralCache{upto: 0.0, result: (0.0, 0.0)};
 
             't_loop:
             for i in dots_per_thread*j..dots_per_thread*(j+1) {
@@ -42,7 +43,7 @@ fn main() {
                     }
                 };
 
-                let (spiral_x, spiral_y) = line_to_spiral(scale_t_for_spiral(t));
+                let (spiral_x, spiral_y) = line_to_spiral(scale_t_for_spiral(t), &mut integral_cache);
                 let (output_x, output_y) = ((spiral_x * size/2.0 + size/2.0) as f32, (spiral_y * size/2.0 + size/2.0) as f32);
 
                 let (zoomed_x, zoomed_y) = get_zoomed_coords(output_x, output_y, size, focused_area_size, spiral_center);
@@ -71,19 +72,24 @@ fn line_to_long_lat(t: f64, g: f64) -> (f64, f64) {
     (((((t + 180.0) % 360.0) + 360.0) % 360.0) - 180.0,      t * g / 360.0)
 }
 
-fn line_to_spiral(t: f64) -> (f64, f64) {
-    (Integrator::new(|s: f64| (s * s).cos())
-        .max_iters(1000)
+fn line_to_spiral(t: f64, cache: &mut IntegralCache) -> (f64, f64) {
+    let out = (Integrator::new(|s: f64| (s * s).cos())
+        .max_iters(200000000)
         .points(&*Points::new())
-        .run(0.0..t)
+        .run(cache.upto..t)
         .estimate()
-        .unwrap(),
+        .unwrap() + cache.result.0,
     Integrator::new(|s: f64| (s * s).sin())
-         .max_iters(1000)
+         .max_iters(200000000)
          .points(&*Points::new())
-         .run(0.0..t)
+         .run(cache.upto..t)
          .estimate()
-         .unwrap())
+         .unwrap() + cache.result.1);
+
+    cache.upto = t;
+    cache.result = out;
+
+    out
 }
 
 // take better measurements
@@ -100,4 +106,9 @@ fn get_zoomed_coords(x: f32, y: f32, overall_size: f64, zoomed_size: f64, center
     let zoomed_diff = (-spiral_center + zoomed_size / 2.0) as f32;
 
     (x + zoomed_diff, y + zoomed_diff)
+}
+
+struct IntegralCache {
+    upto: f64,
+    result: (f64, f64)
 }
